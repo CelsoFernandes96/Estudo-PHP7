dd5d73cb8537338109740a4de5ecd4f51366c50c
Celso Fernandes <celsojfernandes96@gmail.com>
2018-11-01 22:57:16 -0300

    Admin - Categorias

--- Estudo-PHP7/ecommerce/index.php
+++ Estudo-PHP7/ecommerce/index.php
@@ -6,6 +6,7 @@ use Slim\Slim;
 use Hcode\Page;
 use Hcode\PageAdmin;
 use \Hcode\Model\User;
+use \Hcode\Model\Category;
 
 $app = new Slim();
 
@@ -157,6 +158,62 @@ $app->post('/admin/forgot/reset', function() {
 		"footer" => false
 	]);
 	$pageResetSuccess->setTpl("forgot-reset-success");
+});
+
+$app->get('/admin/categories', function() {
+	User::verifyLogin();
+	$categories = Category::listAll();
+
+	$pageCategories = new PageAdmin();
+	$pageCategories->setTpl("categories",[
+		"categories" => $categories
+	]);
+});
+
+$app->get('/admin/categories/create', function() {
+	User::verifyLogin();
+	$pageCategories = new PageAdmin();
+	$pageCategories->setTpl("categories-create");
+});
+
+$app->post('/admin/categories/create', function() {
+	User::verifyLogin();
+	$category = new Category();
+	$category->setData($_POST);
+	$category->save();
+	header("Location: /admin/categories");
+	exit;
+});
+
+$app->get('/admin/categories/:idcategory/delete', function($idcategory) {
+	User::verifyLogin();
+	$category = new Category();
+	$category->get((int)$idcategory);
+	$category->delete();
+
+	header("Location: /admin/categories");
+	exit;
+});
+
+$app->get('/admin/categories/:idcategory', function($idcategory) {
+	User::verifyLogin();
+	$category = new Category();
+	$category->get((int)$idcategory);
+	$pageCategories = new PageAdmin();
+	$pageCategories->setTpl("categories-update", [
+		"category" => $category->getValues()
+	]);
+});
+
+$app->post('/admin/categories/:idcategory', function($idcategory) {
+	User::verifyLogin();
+	$category = new Category();
+	$category->get((int)$idcategory);
+	$category->setData($_POST);
+	$category->save();
+
+	header("Location: /admin/categories");
+	exit;
 });
 
 $app->run();
