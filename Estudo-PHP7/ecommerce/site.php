f53311530de62621739cacea0a6d7cb177396278
Celso Fernandes <celsojfernandes96@gmail.com>
2018-11-07 00:28:41 -0200

    Carrinho de compras

--- Estudo-PHP7/ecommerce/site.php
+++ Estudo-PHP7/ecommerce/site.php
@@ -5,6 +5,7 @@ use Hcode\PageAdmin;
 use \Hcode\Model\User;
 use \Hcode\Model\Category;
 use \Hcode\Model\Product;
+use Hcode\Model\Cart;
 
 $app->get('/', function() {
 	$products = Product::listAll();
@@ -47,6 +48,12 @@ $app->get('/products/:desurl', function($desurl) {
 		"product" => $product->getValues(),
 		"categories" => $product->getCategories()
 	]);
+});
+
+$app->get('/cart', function() {
+	$cart = Cart::getFromSession();
+	$page = new Page();
+	$page->setTpl("cart");
 });
 
 ?>
